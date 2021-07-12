3310f13948e733382eba1b900c4c301cdb858bbf
pickerxxr <18307100052@fudan.edu.cn>
2021-07-12 20:48:40 +0800

    add data finish dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,6 +1,11 @@
 # include <iostream>
+// # include <bits/stdc++.h>
+# include <vector>
+# include <queue>
 
 using namespace std;
+# define mx 20010
+vector<int> edge[mx], cost[mx];
 
 struct graph_data{
     int id, dist;
@@ -8,6 +13,7 @@ struct graph_data{
         return dist > p.dist;
     }
 };
+
 
 // class graph_data{
 //     public:
@@ -27,8 +33,52 @@ struct books {
     char author[50];
 };
 
+int dijkstra(int source, int dest){
+    int d[mx];
+    for (int i = 0 ; i <= mx; i++){
+        d[i] = 99999999;
+    }
+    // priority queue def
+    priority_queue<graph_data> q;
+    graph_data u, v;
+    u.id = source;
+    u.dist = 0;
+    q.push(u);
+    d[source] = 0;
+
+    while (!q.empty()) {
+        u = q.top();
+        q.pop();
+        int u_cost = d[u.id];
+
+        for (int i = 0; i < edge[u.id].size(); i++){
+            v.id = edge[u.id][i]; v.dist = cost[u.id][i] + u.dist;
+            if (d[v.id] > v.dist) {
+            d[v.id] = v.dist;
+            q.push(v);
+            }
+        }
+    }
+    return d[dest];
+}
+
 int main(){
     // graph_data g1(0, 12);
-    cout << g1.dist << endl;
+    int v, e, src, dest;
+    while (scanf(("%d\t%d"), &v, &e) == 2){
+        while (e--){
+            int u, v, weight;
+            cin >> u >> v >> weight;
+            edge[u].push_back(v);
+            edge[v].push_back(u);
+            cost[u].push_back(weight);
+            cost[v].push_back(weight);
+        }
+        cin >> src >> dest;
+        int ans = dijkstra(src, dest);
+        cout << "the sssp of " << src << "and " << dest << " is " << ans << endl;
+    };
+    cout << "hello~" << endl;
     return 0;
+
 }
