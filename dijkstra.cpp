03dcca3bdd4d16d67661db86e1e1d4b6277e4ac5
pickerxxr <18307100052@fudan.edu.cn>
2021-07-12 11:18:30 +0800

    fix

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,23 +2,23 @@
 
 using namespace std;
 
-// struct graph_data{
-//     int id, dist;
-//     bool operator < (const graph_data &p ) const {
-//         return dist > p.dist;
-//     }
-// };
-
-class graph_data{
-    public:
-        int id;
-        int dist;
-    
-    graph_data(int id_num, int dist_num){
-        id = id_num;
-        dist = dist_num;
+struct graph_data{
+    int id, dist;
+    bool operator < (const graph_data &p ) const {
+        return dist > p.dist;
     }
 };
+
+// class graph_data{
+//     public:
+//         int id;
+//         int dist;
+    
+//     graph_data(int id_num, int dist_num){
+//         id = id_num;
+//         dist = dist_num;
+//     }
+// };
     
 
 struct books {
@@ -28,7 +28,7 @@ struct books {
 };
 
 int main(){
-    graph_data g1(0, 12);
+    // graph_data g1(0, 12);
     cout << g1.dist << endl;
     return 0;
 }
