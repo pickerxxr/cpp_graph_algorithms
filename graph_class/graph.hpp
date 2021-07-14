7bad60ce5487763a77a25c48b0564ef83952fea9
pickerxxr <18307100052@fudan.edu.cn>
2021-07-14 10:15:57 +0800

    rebuilding this pac 2021年 7月14日 星期三 10时15分57秒 CST

--- graph_class/graph.hpp
+++ graph_class/graph.hpp
@@ -58,4 +58,44 @@ public:
 
 
 
+class mem_graph_t{
 
+public:
+  vid_t num_vertices;
+  size_t nedges;
+  vid_t *neighbors;
+  std::vector<mem_adjlist_t> vdata;
+  vid_t high_degree_threshold; // starting from which degree is a node considered a high-degree node (if exceeded)
+  double high_degree_factor; // average degree * hdf = hdt
+  std::fstream h2h_file; // file that keeps edges between two high-degree vertices on external memory
+  std::fstream low_degree_file; // file that keeps edges incident to a low-degree vertex on external memory
+  size_t num_h2h_edges;
+
+
+public:
+  mem_graph_t() : num_vertices(0), nedges(0), high_degree_factor(0), high_degree_threshold(0), num_h2h_edges(0) {  }
+
+  ~mem_graph_t()
+  {
+        if (neighbors){
+            free(neighbors);
+        }
+  }
+
+  void resize(vid_t _num_vertices)
+  {
+      num_vertices = _num_vertices;
+      vdata.resize(num_vertices);
+  }
+
+  size_t num_edges() const { return nedges; }
+
+  size_t stream_build(std::ifstream &fin, size_t num_edges, dense_bitset &is_high_degree, dense_bitset &has_high_degree_neighbor, std::vector<size_t> &count, bool write_low_degree_edgelist);
+  mem_adjlist_t &operator[](size_t idx) { return vdata[idx]; };
+
+  const mem_adjlist_t &operator[](size_t idx) const {
+		return vdata[idx];
+	}
+
+
+};
